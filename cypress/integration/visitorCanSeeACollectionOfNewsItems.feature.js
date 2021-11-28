6de45b83fe8e7af487cf3dc59c87a2b65bb8ff82
gsim1903 <gavin@internetbusker.co>
2021-11-28 09:26:15 +0100

    test for top headlines added

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,5 +1,50 @@
-describe('', () => {
-  it('', () => {
-    cy.visit('/')
-  });
+describe('visting the application a user..', () => {
+  beforeEach(() => { 
+    
+    cy.visit('/')})
+    it('is expected to see an appropriate header', () => {
+      cy.get('h1').should('contain.text', 'The Headlines Today')
+    })
+
+    it.only('is expected to see 5 news items', () => {
+      cy.get('[data-cy=news-list]').children().should('have.length',3)
+    })
+
+    it.only('is expected news items will be from today', () => {
+       cy.get('[data-cy=date]').should('contain', "28.11.2021")
+     })
+     
 });
+
+describe('visting the application a user..', () => {
+  beforeEach(() => { cy.visit('/')
+  
+  it ("is expected to display the first news story", () => {
+    cy.get("#story-1").within(() => {
+        cy.get(".image").should("exist")
+        cy.get(".header").should("contain", "My First News Story")
+        cy.get(".description").should("contain", "This is about story 1")
+    })
+  })
+
+    it ("is expected to display the second news story", () => {
+      cy.get("#story-1").within(() => {
+          cy.get(".image").should("exist")
+          cy.get(".header").should("contain", "My Second News Story")
+          cy.get(".description").should("contain", "This is about story 2")
+      })
+    })
+
+    it ("is expected to display the third news story", () => {
+      cy.get("#story-1").within(() => {
+          cy.get(".image").should("exist")
+          cy.get(".header").should("contain", "My Third News Story")
+          cy.get(".description").should("contain", "This is about story 3")
+      })
+    })
+
+  })
+  
+})
+
+
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -7,7 +7,7 @@ const NewsCard = (props) => {
       image='https://editorial.fxstreet.com/images/Markets/Currencies/Digital%20Currencies/Bitcoin/bitcoins-52602600_Large.jpg'
       header='DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017'
       meta={`By: ${'Lorenzo Stroe'} at ${'FXStreet'}`}
-      description='DBS Bank of Singapore has just announced the launch of a digital asset exchange featuring Bitcoin, Ethereum, Bitcoin Cash, and XRP and several fiat cu'
+      description='DBS Bank of Simmie has just announced the launch of a digital asset exchange featuring Bitcoin, Ethereum, Bitcoin Cash, and XRP and several fiat cu'
       extra={<Header as='h6'>`Published at ${"2020-12-10T20:29:20Z"}`</Header>}
 
     />
